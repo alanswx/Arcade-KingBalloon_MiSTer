1ac342e8b8ce6d368b0f8d598c21bd2c99792d76
Alan Steremberg <alan.steremberg@gmail.com>
2019-08-15 18:49:32 -0700

    first check in

---
+++ releases/build_rom_KingBalloon.bat
@@ -0,0 +1,42 @@
+@echo off
+
+
+set    zip=kingball.zip
+set ifiles=prg1.7f+prg2.7j+prg3.7l+prg3.7l+prg3.7l+prg3.7l+chg1.1h+chg2.1k+kb2-1
+
+set  ofile=a.kingballoon.rom
+
+rem =====================================
+setlocal ENABLEDELAYEDEXPANSION
+
+set pwd=%~dp0
+echo.
+echo.
+
+if EXIST %zip% (
+
+	!pwd!7za x -otmp %zip%
+	if !ERRORLEVEL! EQU 0 ( 
+		cd tmp
+
+		copy /b/y %ifiles% !pwd!%ofile%
+		if !ERRORLEVEL! EQU 0 ( 
+			echo.
+			echo ** done **
+			echo.
+			echo Copy "%ofile%" into root of SD card
+		)
+		cd !pwd!
+		rmdir /s /q tmp
+	)
+
+) else (
+
+	echo Error: Cannot find "%zip%" file
+	echo.
+	echo Put "%zip%", "7za.exe" and "%~nx0" into the same directory
+)
+
+echo.
+echo.
+pause
